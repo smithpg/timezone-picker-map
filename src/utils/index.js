7d862d4baa26a05f97700fe28f46f670c563cb72
Jeff Spingeld <jeff.spingeld@gmail.com>
2020-08-29 00:26:48 -0400

    Finish testing/fixing split function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,56 +1,49 @@
 export const alternate = (array1, array2) => {
-  const shorterLength = Math.min(array1.length, array2.length);
-  let result = [];
-  for (let i = 0; i < shorterLength; i++) {
-    result.push(array1[i], array2[i]);
-  }
-  result.push(...array1.slice(shorterLength), ...array2.slice(shorterLength));
-  return result;
+    const shorterLength = Math.min(array1.length, array2.length);
+    let result = [];
+    for (let i = 0; i < shorterLength; i++) {
+        result.push(array1[i], array2[i]);
+    }
+    result.push(...array1.slice(shorterLength), ...array2.slice(shorterLength));
+    return result;
 };
 
 // TODO: change name to...something. maybe splitBy?
-export const splitIntoBoundedGroups = (text, matchIndices) => {
-  let nonMatching = [];
-  let matching = [];
+export const splitIntoBoundedGroups = (text, matchRanges) => {
+    const arr = [];
 
-  // TODO: change to the below, no need for initial empty match
-  // if (matchIndices[0][0] !== 0) {
-  //     const firstNonMatchingSubstring = text.slice(0, matchIndices[0][0]);
-  //     nonMatching.push(firstNonMatchingSubstring);
-  // }
-
-  const firstNonMatchingSubstring = text.slice(0, matchIndices[0][0]);
-
-  if (firstNonMatchingSubstring !== "") {
-    nonMatching.push(firstNonMatchingSubstring);
-  }
-
-  matchIndices.forEach((indexPair, indexPairIndex) => {
-    const nextPair = matchIndices[indexPairIndex + 1];
-    const start = indexPair[0];
-    const end = indexPair[1] + 1;
-
-    matching.push(text.slice(start, end));
-
-    if (!!nextPair) {
-      nonMatching.push(text.slice(end, nextPair[0]));
-    } else {
-      nonMatching.push(text.slice(end));
+    if (matchRanges.length === 0) {
+        return [{ matches: false, text }];
     }
-  });
 
-  // TODO: this happens later outside of this function
-  // prob something like: matches.map(m => m.matches ? `<span className="match">${m}</span>` : m)
-  // matching = matching.map(
-  //     (substring) => `<span className="match">${substring}</span>`
-  // );
+    if (matchRanges[0][0] !== 0) {
+        const firstNonMatchingSubstring = text.slice(0, matchRanges[0][0]);
+        arr.push({ matches: false, text: firstNonMatchingSubstring });
+    }
 
-  const arr = alternate(
-    nonMatching.map((nm) => ({ matches: false, text: nm })),
-    matching.map((m) => ({ matches: true, text: m }))
-  );
-  return arr;
-  // const finalHtml = alternate(nonMatching, matching).join('');
-  // const finalHtmlArray = alternate(nonMatching, matching);
-  // return finalHtml;
+    matchRanges.forEach((indexPair, indexPairIndex) => {
+        const start = indexPair[0];
+        const end = indexPair[1] + 1;
+        const matchingSubstring = text.slice(start, end);
+        arr.push({ matches: true, text: matchingSubstring });
+
+        const nextMatch = matchRanges[indexPairIndex + 1];
+        if (!!nextMatch) {
+            arr.push({ matches: false, text: text.slice(end, nextMatch[0]) });
+        } else if (text.slice(end) !== '') {
+            arr.push({ matches: false, text: text.slice(end) });
+        }
+    });
+
+    return arr;
+
+    // TODO: this happens later outside of this function
+    // prob something like: matches.map(m => m.matches ? `<span className="match">${m}</span>` : m)
+    // matching = matching.map(
+    //     (substring) => `<span className="match">${substring}</span>`
+    // );
+
+    // const finalHtml = alternate(nonMatching, matching).join('');
+    // const finalHtmlArray = alternate(nonMatching, matching);
+    // return finalHtml;
 };
