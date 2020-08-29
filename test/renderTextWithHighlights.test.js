8058fa25b6a437b30eb7715ca9b66fd9a5a9e9df
smithpg <patrickgromersmith@gmail.com>
2020-08-28 20:43:23 -0700

    misc

--- test/renderTextWithHighlights.test.js
+++ test/renderTextWithHighlights.test.js
@@ -1,46 +1,61 @@
-import { renderTextWithHighlights } from '../src/utils/';
+import { splitIntoBoundedGroups } from "../src/utils/";
 
-describe('renderTextWithHighlights works correctly', () => {
-    test('when the whole string matches', () => {
-        // expect(2 + 2).toBe(4);
+describe("splitIntoBoundedGroups works correctly", () => {
+  test("when the whole string matches", () => {
+    // expect(2 + 2).toBe(4);
+  });
+
+  test("when the whole string does not match", () => {
+    // expect(2 + 2).toBe(4);
+  });
+
+  describe("with all combinations of beginning/end does/does not match", () => {
+    test("beginning matches, end matches", () => {
+      const text = `01234567`;
+      const indices = [
+        [0, 0],
+        [3, 5],
+        [7, 7],
+      ];
+      // expected: <span className="match">l</span>or<span className="match">em </span>i<span className="match">p</span>sum dolor sit
+      const result = splitIntoBoundedGroups(text, indices);
+      const expectedResult = [
+        { matches: true, text: "l" },
+        { matches: false, text: "or" },
+        { matches: true, text: "em " },
+        { matches: false, text: "i" },
+        { matches: true, text: "p" },
+        { matches: false, text: "sum dolor sit" },
+      ];
+      expect(result).toEqual(expectedResult);
     });
 
-    test('when the whole string does not match', () => {
-        // expect(2 + 2).toBe(4);
+    test("beginning matches, end does not match", () => {
+      const text = `lorem ipsum dolor sit`;
+      const indices = [
+        [0, 0],
+        [3, 5],
+        [7, 7],
+      ];
+      // expected: <span className="match">l</span>or<span className="match">em </span>i<span className="match">p</span>sum dolor sit
+      const result = splitIntoBoundedGroups(text, indices);
+      const expectedResult = [
+        { matches: true, text: "l" },
+        { matches: false, text: "or" },
+        { matches: true, text: "em " },
+        { matches: false, text: "i" },
+        { matches: true, text: "p" },
+        { matches: false, text: "sum dolor sit" },
+      ];
+      expect(result).toEqual(expectedResult);
     });
 
-    describe('with all combinations of beginning/end does/does not match', () => {
-        test('beginning matches, end matches', () => {
-            // expect(2 + 2).toBe(4);
-        });
-
-        test('beginning matches, end does not match', () => {
-            const text = `lorem ipsum dolor sit`;
-            const indices = [
-                [0, 0],
-                [3, 5],
-                [7, 7],
-            ];
-            // expected: <span className="match">l</span>or<span className="match">em </span>i<span className="match">p</span>sum dolor sit
-            const result = renderTextWithHighlights(text, indices);
-            const expectedResult = [
-                { matching: false, text: '' },
-                { matching: true, text: 'l' },
-                { matching: false, text: 'or' },
-                { matching: true, text: 'em ' },
-                { matching: false, text: 'i' },
-                { matching: true, text: 'p' },
-                { matching: false, text: 'sum dolor sit' },
-            ];
-            expect(result).toEqual(expectedResult);
-        });
-
-        test('beginning does not match, end matches', () => {
-            // expect(2 + 2).toBe(4);
-        });
-
-        test('beginning does not match, end does not match', () => {
-            // expect(2 + 2).toBe(4);
-        });
+    test("beginning does not match, end matches", () => {
+      // expect(2 + 2).toBe(4);
     });
+
+    test("beginning does not match, end does not match", () => {
+      // expect(2 + 2).toBe(4);
+    });
+  });
 });
